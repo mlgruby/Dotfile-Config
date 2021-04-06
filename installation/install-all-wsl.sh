3e80e8754ae2c17abad1f6efbc8ab246018ff3cc
Satyasheel <satyasheel@quantexa.com>
2021-04-06 08:56:01 +0100

    housekeeping

--- installation/install-all-wsl.sh
+++ installation/install-all-wsl.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo '[*] Making all installation script executable ...'
 chmod +x *.sh
+echo '[*] Installing fzf ...'
 ./fzf-installation.sh
+echo '[*] Installing coursier cli ...'
 ./coursier-cli-installation.sh
+echo '[*] Installing forgit ...'
 ./forgit-installation.sh
-./fzf-installation.sh
+echo '[*] Starting installation of neovim and required plugins ...'
 ./neovim-installation.sh
+echo '[*] Installing zsh and oh-my-zsh ...'
+./zsh-installation.sh
+echo '[*] Installing zsh required autocomplition plugins ...'
+./zsh-plugin-installation.sh
+echo '[*] Installing zsh poweline10k theme ...'
 ./powerlevel10k-theme-installation.sh
+echo '[+]Done, Your terminal is ready with zsh and neovim configurations!'
--- stow/stow-nix.sh
+++ stow/stow-nix.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo '[*] Removing default configurations ...'
 rm ~/.profile ~/.zprofile ~/.gitconfig ~/.aliases ~/.config/git/ignore ~/.config/htop/htoprc
 
+echo '[*] Stowing/Creating simlinks for aliases, git, htop, zsh, neovim & zprofile ...'
 stow -vSt ~ bash htop zsh neovim
 cd git && stow -vSt ~ space && cd ..
 cd shell && stow -vSt ~ nix && cd ..
+
+echo '[*] Installing/Updating Neovim plugins ...'
+nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
